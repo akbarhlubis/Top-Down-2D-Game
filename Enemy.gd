41a1472b2990da80a041c28dfd671e9e4aec21fc
akbarhlubis <alubis87@gmail.com>
2022-10-30 09:12:53 +0700

    first commit

---
+++ Enemy.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+var motion = Vector2()
+
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	var Player = get_parent().get_node("Player")
+	position +=(Player.position - position)/50
+	look_at(Player.position)
+	
+	move_and_collide(motion)
